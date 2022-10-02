a8f0338553ad0a22ea80c51fdf3eda816044027c
ranjinimbalkar4 <ranjitnimbalkar4@gmail.com>
2022-10-02 14:30:41 +0530

    Create thread with Runnable

---
+++ src/main/java/ways/to/create/thread/in/java/ThreadWithRunnable.java
@@ -0,0 +1,37 @@
+package ways.to.create.thread.in.java;
+
+public class ThreadWithRunnable {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //Anonymous Class
+        Thread t = new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Inside new Thread from Runnable - Anonymous class");
+        });
+
+        t.setDaemon(true); // Background thread - main thread does not wait for it.
+        t.start();
+        System.out.println("Staged t as background thread no need to wait...");
+
+        Thread t1 = new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Inside new Thread from Runnable - Lambda");
+        });
+        t1.setDaemon(true);
+        t1.start();
+        System.out.println("Waiting for t1 to completed..");
+        t1.join();
+
+        System.out.println("Exiting main thread...");
+
+    }
+}
